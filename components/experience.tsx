c5d6bb610ee1c47988f2413418a8086fe05f6910
Mohammad Faizan Shaikh <skfaizan2301@gmail.com>
2025-12-08 00:08:06 +0400

    Updated code

--- components/experience.tsx
+++ components/experience.tsx
@@ -57,7 +57,7 @@ export default function Experience() {
     visible: {
       opacity: 1,
       x: 0,
-      transition: { duration: 0.5, ease: "easeOut" },
+      transition: { duration: 0.5, ease: "easeOut" as const },
     },
   };
 
